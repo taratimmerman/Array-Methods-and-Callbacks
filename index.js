56be1256e745527841a2a94d00d48c21b24b9bf4
taratimmerman <timmermantara@gmail.com>
2020-11-10 16:56:25 -0700

    4 failed, 2 passed, 6 total

--- index.js
+++ index.js
@@ -22,11 +22,14 @@ console.log('Task 1 e:', finals2014[0]['Win conditions']);
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+    let finalsData = data.filter(function(item) {
+        return item.Stage === 'Final';
+   });
+   return finalsData;
 }
 
-
+console.log(getFinals(fifaData));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
